8927125ad9057ef584fd17c3b65d2c2f1dbcd223
Lev Alekseev <i@djiggo.ru>
2018-04-29 19:39:10 +0300

    1

--- Memcache.php
+++ Memcache.php
@@ -13,17 +13,28 @@ class Memcache
             return $memcache;
         }
 
+        $memcache_servers = \Config::$cache["memcache"];
 
-        $conf = \Config::$cache;
-
-        if (!array_key_exists("memcache", $conf)) {
-            return;
+        if (\Config::$local_site) {
+            if (!class_exists(\Memcache::class)) {
+                return;
+            }
         }
 
-        $conf = \Config::$cache["memcache"];
-
         $memcache = new \Memcache;
-        $memcache->connect($conf["host"], $conf["port"]);
+        foreach ($memcache_servers as $memcache_server) {
+
+            list($host, $port) = explode(':', $memcache_server);
+
+            $result = $memcache->addServer($host, $port, true, 1);
+
+            if (!$result) {
+                throw new \Exception("Can't connect to memcache");
+            }
+
+            $memcache->setCompressThreshold(5000, 0.2);
+        }
+
         return $memcache;
     }
 
@@ -31,18 +42,40 @@ class Memcache
     {
         $memcache = self::getConnection();
 
-        if (!$memcache)
+        if (!$memcache) {
             return;
+        }
 
-        $value = @$memcache->get($key);
+        $full_key = self::getFullKey($key);
+
+        $value = @$memcache->get($full_key);
 
         $value = unserialize($value);
 
         return $value;
     }
 
+    /**
+     * @param string $key
+     * @return string
+     */
+    protected function getFullKey($key)
+    {
+        $full_key = '123-cache-' . $key;
+        $full_key = md5($full_key);
+
+        return $full_key;
+    }
+
+
     public static function setVarByKey($key, $value, $lifetime = null)
     {
+
+        return;
+        // GET ONLY!
+        // TODO
+
+
         if ($lifetime == 0) {
             return '';
         }
@@ -51,32 +84,37 @@ class Memcache
         if (!$memcache)
             return;
 
+
         $conf = \Config::$cache["memcache"];
         $value = serialize($value);
 
         if ($lifetime === null) {
             $lifetime = $conf['lifetime'];
         }
-        $key = "inst2" . $key;
-        $var_key = @$memcache->set($key, $value, false, $lifetime);
+        $full_key = self::getFullKey($key);
+
+        $var_key = @$memcache->set($full_key, $value, MEMCACHE_COMPRESSED, $lifetime);
 
         return $var_key;
     }
 
     public static function deleteVarByKey($key)
     {
+        return;
+        // GET ONLY!
+        // TODO
+
         $memcache = self::getConnection();
 
         if (!$memcache) {
             return;
         }
 
-        @$memcache->set($key, null, false, 1);
+        $full_key = self::getFullKey($key);
 
-        $memcache->delete($key);
+        @$memcache->set($full_key, null, false, 1);
 
-        self::getVarByKey($key);
-
+        $memcache->delete($full_key);
 
         return true;
 
