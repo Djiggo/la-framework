47c05328313affc728a636137deda25117c6ef57
Lev Alekseev <Djiggo@gmail.com>
2021-05-28 22:03:18 +0300

    remove try catch

--- DB/DBWrapper.php
+++ DB/DBWrapper.php
@@ -12,14 +12,12 @@ class DBWrapper extends \PDOStatement
     public static function getVar($db_name, $query, $params = array())
     {
         $dbh = DBFactory::getDBConn($db_name);
-        try {
-            $stmt = $dbh->prepare($query);
-            $stmt->execute($params);
-            $row = $stmt->fetch(\PDO::FETCH_NUM);
-            return $row[0];
-        } catch (\PDOException $e) {
-            throw new \Exception($e->getMessage());
-        }
+
+        $stmt = $dbh->prepare($query);
+        $stmt->execute($params);
+        $row = $stmt->fetch(\PDO::FETCH_NUM);
+        return $row[0];
+
     }
 
     /**
@@ -29,13 +27,11 @@ class DBWrapper extends \PDOStatement
     public static function getRow($db_name, $query, $params = array())
     {
         $dbh = DBFactory::getDBConn($db_name);
-        try {
-            $stmt = $dbh->prepare($query);
-            $stmt->execute($params);
-            return $stmt->fetchObject();
-        } catch (\PDOException $e) {
-            throw new \Exception($e->getMessage());
-        }
+
+        $stmt = $dbh->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetchObject();
+
     }
 
     /**
@@ -45,13 +41,11 @@ class DBWrapper extends \PDOStatement
     public static function getColomn($db_name, $query, $params = array())
     {
         $dbh = DBFactory::getDBConn($db_name);
-        try {
-            $stmt = $dbh->prepare($query);
-            $stmt->execute($params);
-            return $stmt->fetchAll(\PDO::FETCH_COLUMN);
-        } catch (\PDOException $e) {
-            throw new \Exception($e->getMessage());
-        }
+
+        $stmt = $dbh->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetchAll(\PDO::FETCH_COLUMN);
+
     }
 
     /**
@@ -61,13 +55,11 @@ class DBWrapper extends \PDOStatement
     public static function getRows($db_name, $query, $params = array())
     {
         $dbh = DBFactory::getDBConn($db_name);
-        try {
-            $stmt = $dbh->prepare($query);
-            $stmt->execute($params);
-            return $stmt->fetchAll(\PDO::FETCH_CLASS);
-        } catch (\PDOException $e) {
-            throw new \Exception($e->getMessage());
-        }
+
+        $stmt = $dbh->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetchAll(\PDO::FETCH_CLASS);
+
     }
 
 
@@ -78,13 +70,11 @@ class DBWrapper extends \PDOStatement
     public static function insert($db_name, $query, $params = array())
     {
         $dbh = DBFactory::getDBConn($db_name);
-        try {
-            $stmt = $dbh->prepare($query);
-            $stmt->execute($params);
-            return $dbh->lastInsertId();
-        } catch (\PDOException $e) {
-            throw new \Exception($e->getMessage());
-        }
+
+        $stmt = $dbh->prepare($query);
+        $stmt->execute($params);
+        return $dbh->lastInsertId();
+
     }
 
     /**
@@ -94,27 +84,23 @@ class DBWrapper extends \PDOStatement
     public static function remove($db_name, $query, $params = array())
     {
         $dbh = DBFactory::getDBConn($db_name);
-        try {
-            $stmt = $dbh->prepare($query);
-            $stmt->execute($params);
-            if (!$stmt->rowCount()) {
-                throw  new \Exception('No row was not deleted');
-            }
-        } catch (\PDOException $e) {
-            throw new \Exception($e->getMessage());
+
+        $stmt = $dbh->prepare($query);
+        $stmt->execute($params);
+        if (!$stmt->rowCount()) {
+            throw  new \Exception('No row was not deleted');
         }
+
     }
 
     public static function update($db_name, $query, $params = array())
     {
         $dbh = DBFactory::getDBConn($db_name);
-        try {
-            $stmt = $dbh->prepare($query);
-            $stmt->execute($params);
-            return $stmt->rowCount();
-        } catch (\PDOException $e) {
-            throw new \Exception($e->getMessage());
-        }
+
+        $stmt = $dbh->prepare($query);
+        $stmt->execute($params);
+        return $stmt->rowCount();
+
     }
 
 }
